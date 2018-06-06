f404904226765b2ac4a1aa62e1092bc8224c0a87
Alexandra Preda <predaalexanddra@gmail.com>
2018-06-06 19:34:03 +0300

    first commit

--- src/main/java/ro/ase/contranager/Contranager/repositories/ContractRepo.java
+++ src/main/java/ro/ase/contranager/Contranager/repositories/ContractRepo.java
@@ -1,7 +1,12 @@
 package ro.ase.contranager.Contranager.repositories;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ro.ase.contranager.Contranager.entities.Contract;
 
 public interface ContractsRepo extends JpaRepository<Contract, Long> {
+
+  @Query("select c from Contract c order by c.value desc ")
+   List<Contract> findContractsOrdered();
 }
